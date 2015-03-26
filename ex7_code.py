7fc6e7469ae2451a7fd600f2249adee284855e1a
Stefanos Laskaridis <laskstef@lab.grid.auth.gr>
2015-03-26 16:28:20 +0200

    Fixed ex7_code.py

--- ex7_code.py
+++ ex7_code.py
@@ -1,14 +1,37 @@
 from zipfile import *
+import os
 
-z_file = ZipFile("test_zip.zip")
+# Relevant files
+input_zip_file = 'test_zip.zip'
+output_file = 'test_zip'
+dictionary_file = 'english.txt'
 
-with open('english.txt') as f:
+z_file = ZipFile(input_zip_file)
+
+# cleanup
+if os.path.isfile(output_file):
+    os.remove(output_file)
+
+# read dictionary
+with open(dictionary_file) as f:
     content = f.readlines()
-    
-# print(content[1])
+
+# bruteforce from dictionary
+secret_key = ''
 for password in content:
     try:
         print(password)
         z_file.extractall(pwd=str.encode(password.strip()))
-    except RuntimeError:
+    except:
         pass
+    if os.path.isfile(output_file) and os.path.getsize(output_file)!=0:
+        secret_key = password;
+        break # key found
+    else:
+        continue
+
+# Output
+print('Secret key is: '+secret_key)
+print('Contents of file:')
+with open(output_file, 'r') as file:
+    print(file.read())
